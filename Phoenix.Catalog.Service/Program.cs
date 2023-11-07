1bb0c97c867542154177623f5ee9f99ee61f5bd3
AngelBrambila91 <malthael.91@gmail.com>
2023-11-07 09:21:41 -0600

    fixed Post Problem

--- Phoenix.Catalog.Service/Program.cs
+++ Phoenix.Catalog.Service/Program.cs
@@ -18,6 +18,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers(options =>
+    {
+        options.SuppressAsyncSuffixInActionNames = false;
+    });
 
 var app = builder.Build();
 
